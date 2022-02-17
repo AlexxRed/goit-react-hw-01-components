cf04657d9363924e584304601daa2f1d84f76d9e
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-02-17 14:52:10 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import Profile from "./Profile/Profile";
 import dataUser from '../components/Profile/user.json'
-// import Statistics from "./Statistics/Statistics";
 import dataStatistics from '../components/Statistics/data.json'
 import StatisticList from './Statistics/StatisticList'
 import Section from './Section/Section'
@@ -8,11 +7,7 @@ import FriendList from './FriendList/FriendList'
 import friendsData from '../components/FriendList/friends.json'
 import TransactionHistory from './TransactionHistory/TransactionHistory'
 import transactions from '../components/TransactionHistory/transactions.json'
-// console.log(transactions);
-// console.log(Statistics);
-// console.log(dataStatistics);
-// console.log(friendsData);
-// console.log(FriendList);
+
 export const App = () => {
   return (
     <div>
